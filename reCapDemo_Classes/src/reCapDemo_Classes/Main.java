a06fef05b3cb866d3b1ff5c412c659b879a9424d
mihriban <mihriban@DESKTOP-K586T36>
2022-09-28 19:49:24 +0300

    commit7

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,16 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+	DortIslem dortIslem= new DortIslem();
+	int sonuc =dortIslem.Topla(3, 4);
+	System.out.println(sonuc);
+	int sonuc1 =dortIslem.Cıkar(0, 0);
+	int sonuc2 =dortIslem.Carp(0, 0);
+	int sonuc3=dortIslem.Bol(0,2);
+
+	}
+	
+
+}
